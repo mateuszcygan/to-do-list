d1003006edbbfcd590d0245b645de5183ea39d93
mateuszcygan <mateuszcygan@protonmail.com>
2023-10-26 19:06:52 +0200

    implementation of checkbox next to the task to-do

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
+let createLabel= (input) => {
+    let label = document.createElement('label');
+    label.innerText = input;
+    return label;
+}
+
+let createCheckbox = (label, neededElements) => {
+    for(let i = 0; i < neededElements.length; i++) {
+        label.appendChild(neededElements[i]);
+    }
+    return label;
+}
+
+
 function addNewToDoTask() {
+    //get a list with tasks to do and value of the input in input field
     let ul = document.getElementById("list-to-do");
     let input = document.getElementById("search-box").value;
-    let li = document.createElement('li');
-    ul.appendChild(li).innerText = input;
+
+    let taskListElement = document.createElement('li');
+    let taskLabel = createLabel(input);
+
+    //create elements needed for checkbox
+    let taskInput = document.createElement('input');
+    taskInput.type = "checkbox";
+
+    let taskSpan = document.createElement('span');
+    taskSpan.class = "checkmark";
+
+    let checkboxElements = [taskInput, taskSpan];
+
+    taskLabel = createCheckbox(taskLabel, checkboxElements);
+
+    taskListElement.appendChild(taskLabel);
+    ul.appendChild(taskListElement);
 }
+
