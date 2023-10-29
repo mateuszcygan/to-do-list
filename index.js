b800df3ea650c943bc8804519ddf36dd59a4973a
mateuszcygan <mateuszcygan@protonmail.com>
2023-10-29 18:00:23 +0100

    the OK button added in pop-up window, function responsible for checking if the input is empty (only whitespaces)

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-let createLabel= (input) => {
-    let label = document.createElement('label');
-    label.innerText = input;
-    return label;
-}
+let createLabel = (input) => {
+  let label = document.createElement("label");
+  label.innerText = input;
+  return label;
+};
 
 let createCheckbox = (label, neededElements) => {
-    for(let i = 0; i < neededElements.length; i++) {
-        label.appendChild(neededElements[i]);
-    }
-    return label;
+  for (let i = 0; i < neededElements.length; i++) {
+    label.appendChild(neededElements[i]);
+  }
+  return label;
+};
+
+function containsOnlyWhiteSpaces(str) {
+  return !str.replace(/\s/g, "").length;
 }
 
-
 function addNewToDoTask() {
-    //get a list with tasks to do and value of the input in input field
-    let ul = document.getElementById("list-to-do");
-    let input = document.getElementById("search-box").value;
+  //get a list with tasks to do and value of the input in input field
+  let ul = document.getElementById("list-to-do");
+  let input = document.getElementById("search-box").value;
 
-    let taskListElement = document.createElement('li');
+  if (containsOnlyWhiteSpaces(input)) {
+    let warning = document.querySelector(".alert-error");
+    console.log(warning);
+  } else {
+    let taskListElement = document.createElement("li");
     let taskLabel = createLabel(input);
 
     //create elements needed for checkbox
-    let taskInput = document.createElement('input');
+    let taskInput = document.createElement("input");
     taskInput.type = "checkbox";
 
-    let taskSpan = document.createElement('span');
+    let taskSpan = document.createElement("span");
     taskSpan.class = "checkmark";
 
     let checkboxElements = [taskInput, taskSpan];
@@ -33,5 +40,5 @@ function addNewToDoTask() {
 
     taskListElement.appendChild(taskLabel);
     ul.appendChild(taskListElement);
+  }
 }
-
